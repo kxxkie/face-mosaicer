316a59d9af70d0147b7f33b4b29e9ab81dc6f47d
kxxkie <taru6996@gmail.com>
2023-01-19 04:18:12 +0000

    set title tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import { useCallback, useState } from 'react';
 import { Welcome, Editor } from '@/components/organisms';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -9,7 +10,14 @@ const Home: NextPage = () => {
     setFile(null);
   }, []);
 
-  return <>{file ? <Editor file={file} goBack={goBack} /> : <Welcome onUpload={(file) => setFile(file)} />}</>;
+  return (
+    <>
+      <Head>
+        <title>{file ? 'Editor' : 'Welcome'} | face mosaicer</title>
+      </Head>
+      {file ? <Editor file={file} goBack={goBack} /> : <Welcome onUpload={(file) => setFile(file)} />}
+    </>
+  );
 };
 
 export default Home;
