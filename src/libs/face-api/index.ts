026815341bc5adc482ca98d8e74cb28b1e11d536
kxxkie <taru6996@gmail.com>
2023-01-19 06:02:19 +0000

    remove unused model

--- src/libs/face-api/index.ts
+++ src/libs/face-api/index.ts
@@ -4,15 +4,8 @@ export const loadModels = async () => {
   const MODEL_URL = '/models';
   await Promise.all([
     faceapi.loadSsdMobilenetv1Model(MODEL_URL),
-    // faceapi.loadFaceLandmarkModel(MODEL_URL),
     faceapi.loadFaceRecognitionModel(MODEL_URL),
-    faceapi.loadAgeGenderModel(MODEL_URL),
     faceapi.loadFaceExpressionModel(MODEL_URL),
-    // faceapi.nets.tinyFaceDetector.load(MODEL_URL),
-    // faceapi.nets.ageGenderNet.load(MODEL_URL),
-    // faceapi.nets.faceExpressionNet.load(MODEL_URL),
-    // faceapi.nets.faceLandmark68Net.load(MODEL_URL),
-    // faceapi.nets.faceRecognitionNet.load(MODEL_URL),
   ]);
 };
 
